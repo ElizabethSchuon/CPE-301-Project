256fac350fa74160841374c6ac8014ade17505c1
ElizabethSchuon <64823398+ElizabethSchuon@users.noreply.github.com>
2021-05-01 10:36:51 -0700

    commented loop suggestions
    I added a commented-out loop that is part pseudo-code part actual code. It would handle the part about having the water on for 30 seconds and restarting if they take their hand away. It does not handle them having their hands away for 15 seconds but I will add that I also added some comments at the top for comment semantics. And I commented in at the bottom what we need to do to reset.

--- CPE_301_Group_Project.ino
+++ CPE_301_Group_Project.ino
@@ -1,3 +1,15 @@
+/*
+CPE 301 Group Project
+Spring 2021
+Elizabeth Schuon
+Jullian Flienger
+-----------------------------------
+Comment notes:
+All self doccumenting comments with a space betwen the '//' and the comment were made by Jullian
+All self doccumenting comments without one were made by Elizabeth
+Comments directed at eachother in the code are signed with a /ES or /JF corrisponding to the person who wrote them
+*/
+
 // libraries
 #include <LiquidCrystal.h>
 #include "DHT.h"
@@ -18,6 +30,8 @@ const int buttPin = 15;
 
 bool LEDstart = false;
 bool buttPress = false;
+//this is the variable I am using in the loop thing below /ES
+int sensorCheck = 0;
 
 #define DHTTYPE DHT11
 DHT dht(dhtPin, DHTTYPE);
@@ -62,7 +76,7 @@ void loop()
 {
   //button stuff
   while(!buttPress){
-    //occurs as long as the button has not been pressed
+    //occurs as long as the button has not been pressed 
     if(buttPin){
       //make buttPress true so it does not continue to loop
       buttPress = true;
@@ -70,110 +84,129 @@ void loop()
       digitalWrite(motorPin, HIGH);
       
     }
-    //no else condition, nothing should happen if the botton is not pressed
-    // it doesn't matter if the button is pressed more than once, so no debouncing needed
+    //no else condition, nothing should happen if the botton is not pressed 
+    //it doesn't matter if the button is pressed more than once, so no debouncing needed 
   }
   
-  // Temperature
-  // 
-  // 
-  lcd.clear();
-  // read temperature in Fahrenheit
-  float f = dht.readTemperature(true);
+  //needs to be in a loop for if hands are in place, i think that this is all that will need to be coninously updated /ES
+      // Temperature
+      // 
+      // 
+        lcd.clear();
+        // read temperature in Fahrenheit
+        float f = dht.readTemperature(true);
 
-  // check for error
-  if (isnan(f)) 
-  {
-    lcd.print("ERROR");
-    return;
-  }
+        // check for error
+        if (isnan(f)) 
+        {
+          lcd.print("ERROR");
+          return;
+        }
   
-  // print temp to LCD
-  lcd.setCursor(0,0); 
-  lcd.print("Temp = ");
-  lcd.print(f);
-  lcd.print(" F");
-  // 
-  // 
-  // End Temperature
+        // print temp to LCD
+        lcd.setCursor(0,0); 
+        lcd.print("Temp = ");
+        lcd.print(f);
+        lcd.print(" F");
+      // 
+      // 
+      // End Temperature
 
-
-
-  // Ultrasonic Sensor
+      // Ultrasonic Sensor
+      //
+      // 
+        // Write a pulse to the HC-SR04 Trigger Pin
+        digitalWrite(trigPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(trigPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin, LOW);
+  
+        // Measure the response from the HC-SR04 Echo Pin
+        duration = pulseIn(echoPin, HIGH);
+  
+        // Determine distance from duration
+        // Use 343 metres per second as speed of sound
+        distance = duration*0.034/2;
+  
+        if(distance <= 10)
+        {
+          //is this your conditional statement where you turn the green LED on /ES
+          LEDstart = true;
+          digitalWrite(greenLED, HIGH);
+          digitalWrite(yellowLED, LOW);
+        }
+  
+        if(distance > 10 && LEDstart)
+        {
+          digitalWrite(greenLED, LOW);
+          digitalWrite(yellowLED, HIGH);
+          delay(500);
+          digitalWrite(yellowLED, LOW);
+        }
+  
+        // Prints "Distance: <value>" on the second line of the LCD
+        // for debugging purposes right now
+        lcd.setCursor(0,1);
+        lcd.print("Distance: "); 
+        lcd.print(distance);
+        lcd.print(" cm");
+      // 
+      // 
+      // End Ultrasonic Sensor
+  
+  //I am going to make kinda a part code part psudo code loop here to illistrate /ES
   //
-  // 
-  // Write a pulse to the HC-SR04 Trigger Pin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  /*
+    while(sensorCheck <= 30){
+      update the tempurature output of water
+      measure distance with ultrasonic sensor
+      if( hands in range){
+        sensorCheck++;
+      }
+      else{
+        sensorCheck = 0;
+      }
+      delay(1000);
+      //I set it to delay one second /ES
+      //could be more or less frequent if desired /ES
+      //If delay changes the 30 will need to change /ES
+    }
+  */
+  //
+  //end of illustrative loop
   
-  // Measure the response from the HC-SR04 Echo Pin
-  duration = pulseIn(echoPin, HIGH);
+    // RTC Module
+    // 
+    // 
+      DateTime now = rtc.now();
   
-  // Determine distance from duration
-  // Use 343 metres per second as speed of sound
-  distance = duration*0.034/2;
-  
-  
-  if(distance <= 10)
-  {
-    //is this your conditional statement where you turn the green LED on
-    LEDstart = true;
-    digitalWrite(greenLED, HIGH);
-    digitalWrite(yellowLED, LOW);
-  }
-  
-  if(distance > 10 && LEDstart)
-  {
-    digitalWrite(greenLED, LOW);
-    digitalWrite(yellowLED, HIGH);
-    delay(500);
-    digitalWrite(yellowLED, LOW);
-  }
-  
-  
-  
-  
-  // Prints "Distance: <value>" on the second line of the LCD
-  // for debugging purposes right now
-  lcd.setCursor(0,1);
-  lcd.print("Distance: "); 
-  lcd.print(distance);
-  lcd.print(" cm");
-  // 
-  // 
-  // End Ultrasonic Sensor
-
-  
-  // RTC Module
-  // 
-  // 
-  DateTime now = rtc.now();
-
-  
-  Serial.print(now.hour(), DEC);
-  Serial.print(":");
-  Serial.print(now.minute(), DEC);
-  Serial.print(":");
-  Serial.print(now.second(), DEC);
-  Serial.print(" (");
-  Serial.print(now.month(), DEC);
-  Serial.print("/");
-  Serial.print(now.day(), DEC);
-  Serial.print("/");
-  Serial.print(now.year(), DEC);
-  Serial.println(")");
-  // 
-  // 
-  // End RTC Module
+      Serial.print(now.hour(), DEC);
+      Serial.print(":");
+      Serial.print(now.minute(), DEC);
+      Serial.print(":");
+      Serial.print(now.second(), DEC);
+      Serial.print(" (");
+      Serial.print(now.month(), DEC);
+      Serial.print("/");
+      Serial.print(now.day(), DEC);
+      Serial.print("/");
+      Serial.print(now.year(), DEC);
+      Serial.println(")");
+    // 
+    // 
+    // End RTC Module
 
   //resetting stuff
-  
+    //turn off fan
+    //turn off LEDs
+    //I don't know exactly what may need to be turned of for the ultrasonic and tempurature things /ES
+    //turn off any ultrasonic stuff
+    //turn off tempurature display
+    //Turn off tempurature sensor
 
 
-// ignore for now
+// ignore for now /JF
   
   /*if((*pin_? & 1) == 0) // button pin is 0
     // do nothing
